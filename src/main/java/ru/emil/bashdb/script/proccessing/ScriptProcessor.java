ecab7c9e0ad6c2482a40657ed09a84545badb49f
Emil Fataliev <em.fataliev@gmail.com>
2018-02-27 13:19:25 +0300

    boolshit fix

--- src/main/java/ru/emil/bashdb/script/proccessing/ScriptProcessor.java
+++ src/main/java/ru/emil/bashdb/script/proccessing/ScriptProcessor.java
@@ -9,7 +9,7 @@ public interface ScriptProcessor {
   default String readScript(final Path scriptPath) throws IOException {
     return Files.lines(scriptPath)
         .reduce((acc, line) -> acc + line + "\n")
-        .map(String::trim)
+        .map(content -> content.substring(0, content.length() - 1))
         .orElseThrow(IllegalArgumentException::new);
   }
 
